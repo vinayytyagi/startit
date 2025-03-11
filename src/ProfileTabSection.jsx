8976ba5f26672c40db91af6350bbd3e9099e8bc1
Vinay Kumar <tyagivinay807@gmail.com>
2025-03-11 22:39:54 +0530

    committ

--- src/ProfileTabSection.jsx
+++ src/ProfileTabSection.jsx
@@ -271,7 +271,7 @@ function ProfileTabSection() {
               <div className="mt-6 flex justify-end space-x-4">
                 <button
                   onClick={handleCancel}
-                  className="bg-gray-500 text-white px-4 py-2 rounded cursor-pointer"
+                  className="bg-gray-300 text-black px-4 py-2 rounded cursor-pointer"
                 >
                   Cancel
                 </button>
